137161570203f7f8bd183c6ab8f8d584dee21b1e
Daryl3260 <cbhcqnk@icloud.com>
2019-04-25 11:18:44 +0800

    my message

---
+++ Controllers/QuotesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuotesApi.DAO;
+using QuotesApi.Model;
+
+namespace QuotesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        private DataContext _quotesDbContext;
+
+        public QuotesController(DataContext dataContext)//FIXME:什么时候调用的这个构造器？依赖注入，因为只有这一个构造器？
+        {
+            _quotesDbContext = dataContext;
+        }
+
+        // GET: api/Quotes
+        [HttpGet]
+        public IEnumerable<Quote> Get()
+        {
+            return _quotesDbContext.Quotes;
+        }
+
+        // GET: api/Quotes/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Quotes
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Quotes/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DAO/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuotesApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuotesApi.DAO
+{
+    public class DataContext:DbContext
+    {
+        //通过services.AddDbContext注入就需要添加这个构造器
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+        //数据库中返回的结果
+        public DbSet<Quote> Quotes { get; set; }
+    }
+}
